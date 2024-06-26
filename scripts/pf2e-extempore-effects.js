9dfd2142acd62cb35d6a0935b7788733a308decb
Jean-Paul Goodwin <goodwinjeanpaul@outlook.fr>
2024-06-26 21:29:20 +0200

    Try with regexp

--- scripts/pf2e-extempore-effects.js
+++ scripts/pf2e-extempore-effects.js
@@ -426,9 +426,10 @@ function defineDurationFromFrequency(frequency) {
 
 const defineDurationFromTextOfAffliction = (itemDescriptionText) => {
   // an affliction effect's duration is the duration of its first stage!
+  let stageNRegex = new RegExp(`<p>\s*<strong>\s*${localize(`.stageN`).replace('{n}', "\d")}+<\/strong> .+? \((.+)\)\s*<\/p>`)
   const firstStageDurationMatch = itemDescriptionText
     // example: '<p><strong>Stage 1</strong> carrier with no ill effect (1 minute)</p>'
-    .match(/<p>\s*<strong>\s*Stade \d+<\/strong> .+? \((.+)\)\s*<\/p>/)
+    .match(stageNRegex)
   const maximumDurationMatch = itemDescriptionText
     // example: '<p><strong>Maximum Duration</strong> 6 rounds</p>'
     .match(/<p>\s*<strong>\s*Maximum Duration<\/strong> (\d+ rounds?)\s*<\/p>/)
@@ -514,7 +515,8 @@ const isEffectOrCondition = (document) => {
 }
 
 const isAffliction = (itemDescriptionText) => {
-  return itemDescriptionText.match(/<strong>Stade \d+/)
+  let stageNRegex = new RegExp(`<strong>${localize(`.stageN`).replace('{n}', "\d")}+`)
+  return itemDescriptionText.match(stageNRegex)
 }
 
 const isRechargeRoll = (message) => {
@@ -522,7 +524,9 @@ const isRechargeRoll = (message) => {
 }
 
 const calcHighestStageOfAffliction = (itemDescriptionText) => {
-  const stageNumbers = [...itemDescriptionText.matchAll(/[Ss]tade (\d+)/g)].map(m => m[1]).
+  let stageNLocalized = localize(`.stageN`)
+  let stageNRegex = new RegExp(`${stageNLocalized.replace('{n}', "(\d+)").replace(stageNLocalized.charAt(0), `[${stageNLocalized.charAt(0).toUpperCase()}${stageNLocalized.charAt(0).toLowerCase()}]`)}`, 'g')
+  const stageNumbers = [...itemDescriptionText.matchAll()].map(m => m[1]).
     map(numStr => parseInt(numStr))
   return Math.max(...stageNumbers)
 }
